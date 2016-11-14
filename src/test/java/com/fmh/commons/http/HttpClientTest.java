ebeae890fb453453bf3447f16345b663120b40cd
minghui.fan <minghui.fan@generalbiologic.com>
2016-11-14 17:17:02 +0800

    httpClient

--- src/test/java/com/fmh/commons/http/HttpClientTest.java
+++ src/test/java/com/fmh/commons/http/HttpClientTest.java
@@ -8,6 +8,15 @@ public class HttpClientTest {
     public void T1(){
         HttpClient httpClient = new HttpClient();
         HttpResponse httpResponse = httpClient.get("http://news.baidu.com/");
-        System.out.println(httpResponse);
+        System.out.println(httpResponse.getContentCharset());
+        System.out.println(httpResponse.getDocument());
+        System.out.println(httpResponse.getStatusCode());
+    }
+
+    @Test
+    public void T2(){
+        HttpClient httpClient = new HttpClient();
+        HttpResponse httpResponse = httpClient.get("http://news.baidu.com/");
+        System.out.println(httpResponse.getDocument());
     }
 }
