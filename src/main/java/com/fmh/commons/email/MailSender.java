66cf920409b6fe2009e8518aae5c767dd7db5278
minghui.fan <minghui.fan@generalbiologic.com>
2017-02-15 09:22:04 +0800

    email

--- src/main/java/com/fmh/commons/email/MailSender.java
+++ src/main/java/com/fmh/commons/email/MailSender.java
@@ -1,11 +1,13 @@
 package com.fmh.commons.email;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 import java.util.Objects;
 import java.util.Properties;
 
@@ -33,6 +35,42 @@ public class MailSender {
 		message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
 		message.setSubject(subject);
 		message.setContent(content.toString(), "text/html;charset=utf-8");
+		Transport.send(message);
+	}
+
+	public void send(List<String> recipients, String subject, Object content) throws MessagingException {
+		MimeMessage message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(authenticator.getUserAddress()));
+		InternetAddress[] addresses = new InternetAddress[recipients.size()];
+		for (int i=0; i<addresses.length; i++){
+			addresses[i] = new InternetAddress(recipients.get(i));
+		}
+		message.setRecipients(Message.RecipientType.TO, addresses);
+		message.setSubject(subject);
+		message.setContent(content.toString(), "text/html;charset=utf-8");
+		Transport.send(message);
+	}
+
+	public void send(String recipient, String subject, Object content, List<File> attachments) throws MessagingException, UnsupportedEncodingException {
+		MimeMessage message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(authenticator.getUserAddress()));
+		message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+		message.setSubject(subject);
+		Multipart multipart = new MimeMultipart();
+		BodyPart contentPart = new MimeBodyPart();
+		contentPart.setContent(content, "text/html;charset=utf-8");
+		multipart.addBodyPart(contentPart);
+		if (attachments != null) {
+			for (File attachment : attachments) {
+				BodyPart attachmentFile = new MimeBodyPart();
+				DataSource dataSource = new FileDataSource(attachment);
+				attachmentFile.setDataHandler(new DataHandler(dataSource));
+				attachmentFile.setFileName(MimeUtility.encodeWord(attachment.getName()));
+				multipart.addBodyPart(attachmentFile);
+			}
+		}
+		message.setContent(multipart);
+		message.saveChanges();
 		Transport.send(message);
 	}
 
--- src/test/java/com/fmh/commons/email/MailSenderTest.java
+++ src/test/java/com/fmh/commons/email/MailSenderTest.java
@@ -3,6 +3,10 @@ package com.fmh.commons.email;
 import org.junit.Test;
 
 import javax.mail.MessagingException;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by minghui.fan on 2017/2/14.
@@ -13,6 +17,18 @@ public class MailSenderTest {
 	public void test1() throws MessagingException {
 		MailSender mailSender = new MailSender("fmh_hmf@163.com", "fanminghui5193");
 		mailSender.send("971416100@qq.com","测试","测试代码");
+	}
+
+	@Test
+	public void test2() throws MessagingException {
+		MailSender mailSender = new MailSender("fmh_hmf@163.com", "fanminghui5193");
+		mailSender.send(Arrays.asList("971416100@qq.com","fmh_hmf@163.com"),"测试","测试群发邮件");
+	}
+
+	@Test
+	public void test3() throws UnsupportedEncodingException, MessagingException {
+		MailSender mailSender = new MailSender("fmh_hmf@163.com", "fanminghui5193");
+		mailSender.send("971416100@qq.com","测试","测试代码", Arrays.asList(new File("D:/Alibaba Java Development Manual.pdf")));
 	}
 
 }
